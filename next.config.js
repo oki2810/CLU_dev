e2322b7e8606abc75555586ab19f5bc86c241fca
oki <146618117+oki2810@users.noreply.github.com>
2025-07-15 11:22:07 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ module.exports = {
       {
         source: '/api/:path*',
         headers: [
-          { key: 'Access-Control-Allow-Origin', value: 'https://yoshikawa04.github.io' },
+          { key: 'Access-Control-Allow-Origin', value: 'https://oki2810.github.io' },
           { key: 'Access-Control-Allow-Methods', value: 'GET,POST,OPTIONS' },
           { key: 'Access-Control-Allow-Headers', value: 'Content-Type' },
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
