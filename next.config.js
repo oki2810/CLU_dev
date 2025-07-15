9028ee0176b9ac5101224421b034a87bc5fbfbe3
oki <146618117+oki2810@users.noreply.github.com>
2025-07-15 10:22:13 +0900

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,15 @@
+// next.config.js
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Origin', value: 'https://yoshikawa04.github.io' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,POST,OPTIONS' },
+          { key: 'Access-Control-Allow-Headers', value: 'Content-Type' },
+        ],
+      },
+    ]
+  },
+}
